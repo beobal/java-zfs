7b0fd658e1f777e70a95a71d33548df6a311557e
Kohsuke Kawaguchi <kk@kohsuke.org>
2009-03-05 23:09:58 +0000

    [maven-release-plugin]  copy for tag libzfs-0.4
    git-svn-id: https://svn.java.net/svn/zfs~svn@140 caf7eec8-9a76-4577-c562-b5e526361788

---
+++ tags/libzfs-0.4/src/main/java/org/jvnet/solaris/libzfs/jna/zprop_source_t.java
@@ -0,0 +1,31 @@
+/*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
+ *
+ * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+ * or http://www.opensolaris.org/os/licensing.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ */
+
+package org.jvnet.solaris.libzfs.jna;
+
+enum zprop_source_t {
+    // TODO: is this bit mask?
+        ZPROP_SRC_NONE, // = 0x1,
+	ZPROP_SRC_DEFAULT, // = 0x2,
+	ZPROP_SRC_TEMPORARY, // = 0x4,
+	ZPROP_SRC_LOCAL, // = 0x8,
+	ZPROP_SRC_INHERITED // = 0x10
+}
