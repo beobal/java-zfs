f1551c51059ae0e659ea6ebb25af065fafa39c0d
beobal <sam.tunnicliffe@talis.com>
2011-02-10 16:09:46 +0000

    Pruned branches & tags

--- tags/libzfs-0.2/src/main/java/org/jvnet/solaris/libzfs/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * High-level Java API to <tt>libzfs</tt>. Start navigating from {@link LibZFS}.
- */
-package org.jvnet.solaris.libzfs;
--- tags/libzfs-0.4/src/main/java/org/jvnet/solaris/libzfs/jna/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Low level API to <tt>libzfs</tt> through JNA. Not meant to be invoked directly for typical use.
- */
-package org.jvnet.solaris.libzfs.jna;
